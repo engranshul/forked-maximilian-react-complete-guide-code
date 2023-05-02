86460fd25c7a5e08e98d142d7af8b653e91b9fc3
anshul chauhan <anshul101289@gmail.com>
2023-05-02 15:44:50 +0530

    added comments

--- code/03-using-redux-data-in-react-cmp/src/components/Counter.js
+++ code/03-using-redux-data-in-react-cmp/src/components/Counter.js
@@ -17,3 +17,9 @@ const Counter = () => {
 };
 
 export default Counter;
+
+
+// A hook to access the redux store's state. This hook takes a selector function as an argument. The selector is called with the store state.
+
+// This hook takes an optional equality comparison function as the second parameter that allows you to customize the way the selected state is compared to determine whether the component needs to be re-rendered.
+
